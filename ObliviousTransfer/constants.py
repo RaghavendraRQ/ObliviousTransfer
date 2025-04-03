f5cd253c3d2cacadc03b2ca1fc86729b79c7cf78
RaghavnedraRQ <sai783191@gmail.com>
2025-04-03 21:14:11 +0530

    Added main.py for demo. Fixed the CloudTree and MCC remove operation.

--- ObliviousTransfer/constants.py
+++ ObliviousTransfer/constants.py
@@ -1,15 +1,5 @@
-Z = 5
-T = 2
-C = 3
-L = 4
-N = 20
-SEC_PARAM = 5
-
-class Config:
-    TEMPORAL_LOCALITY_WINDOW = 3
-
-    def __init__(self, L, t, Z, temp_window=3):
-        self.L = L
-        self.t = t
-        self.Z = Z
-        self.TEMPORAL_LOCALITY_WINDOW = temp_window
+class SecurityParameters:
+    L = 4
+    STASH_SIZE = 10
+    S = 3
+    KEY_SIZE = 1024
